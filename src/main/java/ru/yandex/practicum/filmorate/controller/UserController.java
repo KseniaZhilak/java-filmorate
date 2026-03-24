9c33085a95f37d42063d95bbd585f98bffde8536
kseniazilak <kzhilak@bk.ru>
2026-03-24 20:28:43 +0300

    add UserController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,90 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.dto.user.CreateUserRequest;
+import ru.yandex.practicum.filmorate.dto.user.UpdateUserRequest;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody CreateUserRequest request) {
+
+        User user = new User();
+
+        user.setEmail(request.getEmail());
+        user.setLogin(request.getLogin());
+        user.setName(request.getName());
+        user.setBirthday(request.getBirthday());
+
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+
+        log.info("Создан пользователь с id={}", user.getId());
+
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody UpdateUserRequest newUser) {
+        User user = findUserOrThrow(newUser.getId());
+            if (newUser.getEmail() != null) {
+                if (newUser.getEmail().equals(user.getEmail())) {
+                    throw new DuplicatedDataException("Этот имейл уже используется");
+                }
+                user.setEmail(newUser.getEmail());
+            }
+
+            if (newUser.getName() != null) {
+                user.setName(newUser.getName());
+            }
+
+            if (newUser.getLogin() != null) {
+                user.setLogin(newUser.getLogin());
+            }
+
+            if(newUser.getBirthday() != null) {
+                user.setBirthday(newUser.getBirthday());
+            }
+
+            return user;
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    private User findUserOrThrow(Long id) {
+        return Optional.ofNullable(users.get(id))
+                .orElseThrow(() -> new NotFoundException("Пользователь не найден"));
+    }
+
+}
